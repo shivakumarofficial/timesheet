f17eed3f8f4706c6238b996b393706bd01e4a482
Shivakumar C <shivakumarchandrappam@gmail.com>
2026-05-28 11:22:29 +0530

    Change STATICFILES_STORAGE to CompressedStaticFilesStorage

--- timesheet_system/settings.py
+++ timesheet_system/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # ================= LOGIN / LOGOUT =================
